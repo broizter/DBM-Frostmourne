faadb1c15822f8e9dd3990b6806d222759cd6c58
broizter <ragu.alvor-0m@icloud.com>
2025-12-04 12:10:09 +0100

    DBM-Core: Bump revision, no code changes

--- DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
+++ DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Anub'Rekhan", "DBM-Naxx", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20250929220131")
+mod:SetRevision("20251203201500")
 mod:SetCreatureID(15956)
 mod:SetEncounterID(1107)
 
