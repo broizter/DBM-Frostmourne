4baf27a1ed029a39d69684ba46b458b428e6cab0
broizter <ragu.alvor-0m@icloud.com>
2026-01-09 15:01:48 +0100

    Core: Bump version to 11.7.0

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20260107231005"),
-	DisplayVersion = "11.6.0", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2025, 09, 25) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("20260109150122"),
+	DisplayVersion = "11.7.0", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2026, 01, 09) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
